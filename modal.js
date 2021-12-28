78788329420385e8709bbdbb8e3c63c65029daf7
Aidana777 <aidananiazalieva27@gmail.com>
2021-12-28 13:32:48 +0600

    first commit

---
+++ modal.js
@@ -0,0 +1,10 @@
+const openmodal=document.getElementById('open');
+const modal_container=document.getElementById('modal-container');
+const closeme=document.getElementById('close_mood');
+
+openmodal.addEventListener ('click', ()=>{
+    modal_container.classList.add('show');
+  });
+closeme.addEventListener ('click',() =>{
+    modal_container.classList.add('show');
+  });
