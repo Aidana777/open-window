fbe119ff96727dfaa7224194fe751ae71d5ae54f
Aidana777 <94773705+Aidana777@users.noreply.github.com>
2022-01-05 14:07:43 +0600

    Add files via upload

--- modal.js
+++ modal.js
@@ -3,8 +3,10 @@ const modal_container=document.getElementById('modal-container');
 const closeme=document.getElementById('close_mood');
 
 openmodal.addEventListener ('click', ()=>{
-    modal_container.classList.add('show');
+    modal_container.classList.add('show')
+    modal_container.style.display= "block"
   });
 closeme.addEventListener ('click',() =>{
-    modal_container.classList.add('show');
+    modal_container.classList.add('show')
+    modal_container.style.display= "none"
   });
